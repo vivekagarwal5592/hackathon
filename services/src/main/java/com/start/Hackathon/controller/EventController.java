14473b0f1db29f2e26dbf4b52bfe3e932e766fce
Vivek Agarwal <vivekagarwal5592@gmail.com>
2018-03-11 20:11:31 -0700

    made demo UI page in ui section of application

--- services/src/main/java/com/start/Hackathon/controller/EventController.java
+++ services/src/main/java/com/start/Hackathon/controller/EventController.java
@@ -1,10 +1,15 @@
 package com.start.Hackathon.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,26 +34,71 @@ public class EventController {
 			@Value("${predix.bearer.token}") final String authorization) {
 		this.url = url;
 		this.authorization = authorization;
-		 restTemplate = new RestTemplate();
+		restTemplate = new RestTemplate();
+
 	}
 
 	@RequestMapping(value = "/getparkingbybbox", method = RequestMethod.POST)
-	public ResponseEntity<parkingevent> getparkingbybbox(@RequestParam String bbox, @RequestParam String startts,
+	public ResponseEntity<parkingevent> getparkingbybbox(@RequestParam String x_position,@RequestParam String y_position, @RequestParam String startts,
 			@RequestParam String endts) {
 
-		return restTemplate.exchange(url + "locations/events?bbox=" + bbox
-				+ "&locationType=PARKING_ZONE&eventType=PKIN&startTime=" + startts + "&endTime=" + endts,
-				HttpMethod.GET, getHeaders(),  parkingevent.class);
-
-		// return restoperations.getForObject(url,companydetails.class);
-	}
 	
-	public HttpEntity<String> getHeaders(){
+		
+		System.out.println(url+"locations/events?bbox="+ getcoordinates(x_position,y_position)
+		+ "&locationType=PARKING_ZONE&eventType=PKIN&startTime="+getdateinTimeStamp(startts)
+		+ "&endTime="+getdateinTimeStamp(endts));
+
+		parkingevent pk = restTemplate
+				.exchange(url+"locations/events?bbox="+ getcoordinates(x_position,y_position)
+						+ "&locationType=PARKING_ZONE&eventType=PKIN&startTime="+getdateinTimeStamp(startts)
+						+ "&endTime="+getdateinTimeStamp(endts), HttpMethod.GET, getHeaders(), parkingevent.class)
+				.getBody();
+
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Access-Control-Allow-Origin","*");
+		return new ResponseEntity<parkingevent>(pk, responseHeaders, HttpStatus.CREATED);
+
+	}
+
+	public HttpEntity<String> getHeaders() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Authorization", "Bearer " + authorization);
 		headers.set("Predix-Zone-Id", "SD-IE-PARKING");
+		// headers.set("Access-Control-Allow-Origin", "*");
 		HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-		return entity;	
+		return entity;
 	}
+
+	public String getdateinTimeStamp(String date) {
+
+		SimpleDateFormat d = new SimpleDateFormat("MM/dd/yyyy");
+
+		try {
+			date = String.valueOf(d.parse(date).getTime());
+			System.out.println(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return date;
+
+	}
+	
+	public String getcoordinates(String x_position,String y_position) {
+		
+		float x1 = Float.parseFloat(x_position)-1;
+		float y1 = Float.parseFloat(y_position)-1;
+		float x2 = Float.parseFloat(x_position)+1;
+		float y2 = Float.parseFloat(y_position)+1;
+		
+		String bbox =String.valueOf(x1) + ":" +String.valueOf(y1) + ","+
+		String.valueOf(x2) + ":" + String.valueOf(y2);
+		
+		return bbox;
+		
+	}
+	
+	
 
 }
